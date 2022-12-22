ad0346dfe01db22aa4ed5321b83ca6e74413b62a
Vivek <vivek2000@iitg.ac.in>
2022-12-22 16:25:11 +0530

    install express js

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const mongoUri = "mongodb://localhost:27017";
+mongoose.set('strictQuery', true);
+const connectToMongo=()=>{
+    mongoose.connect(mongoUri,()=>{
+        console.log("connected to mongodb succssfully")
+    })
+}
+module.exports=connectToMongo;
