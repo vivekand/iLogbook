cbf83bcad8316a73aeeceacbb667b677906e641a
Vivek <vivek2000@iitg.ac.in>
2022-12-23 17:17:45 +0530

    Token added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator"); // this is for user information validation
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET="Whereareyou$going";
 
 // Create a Usser using : POST "/api/auth/createuser" doesn't require Auth
 
@@ -20,7 +23,7 @@ router.post(
       return res.status(400).json({ errors: errors.array() }); // if not true
     }
 
-    try {
+    try {  // used , if there is error then dont add data into the databse..
       // check whether the email exist already
       let user = await User.findOne({ email: req.body.email });
     //   console.log(user);
@@ -29,13 +32,26 @@ router.post(
           .status(400)
           .json({ error: "Sorry a user with this email already exist" });
       }
+      const salt= await bcrypt.genSalt(10); // what does mean by 10 here  // whenever generate responce use await 
+      const secPass= await bcrypt.hash(req.body.password,salt);  // creating hsah..
+
       // creat a new user that will added to database.
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass
       });
-      res.json(user);
+      
+      const data={
+        user:{
+            id:user.id
+        }
+      }
+
+      const authtoken=jwt.sign(data,JWT_SECRET);
+    //   console.log(jwtData);
+     res.json({authtoken})
+    //   res.json(user); 
 
     } catch (error) {
       console.error(error.message);
